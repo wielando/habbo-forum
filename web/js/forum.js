b3f51589996738c6032d02cd07a9675b45fa5c48
Wieland Wüller <wielandfriedrichwuller@gmail.com>
2022-04-12 21:30:24 +0200

    Added Display js logic, added loop to output threads

--- web/js/forum.js
+++ web/js/forum.js
@@ -1,13 +1,41 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    const defaulTab = 'announcement';
+    const defaultTab = 'announcement';
+
+    const announcementEntriesIdentifier = 'announcementEntries';
+    const communityEntriesIdentifier = 'communityEntries';
+
+    const announcementEntriesElement = document.querySelector('[data-tab=' + announcementEntriesIdentifier + ']')
+    const communityEntriesElement = document.querySelector('[data-tab=' + communityEntriesIdentifier + ']')
+
     let currentTab = '';
+    let currentTabEntryIdentifier = '';
 
     const forumTabs = document.querySelectorAll('.forum-tab');
+
+    if (defaultTab === 'announcement') {
+        document.querySelector('#announcementContent').style.display = "block";
+    }
+
+    if (defaultTab === 'community') {
+        document.querySelector('#communityContent').style.display = "block";
+    }
 
     for (const [key, tabElement] of Object.entries(forumTabs)) {
         tabElement.addEventListener('click', (element) => {
             currentTab = element.target.dataset.tabname;
+            currentTabEntryIdentifier = currentTab + 'Entries';
+
+            if (currentTabEntryIdentifier === announcementEntriesIdentifier) {
+                announcementEntriesElement.style.display = "block";
+                communityEntriesElement.style.display = "none";
+            }
+
+            if (currentTabEntryIdentifier === communityEntriesIdentifier) {
+                announcementEntriesElement.style.display = "none";
+                communityEntriesElement.style.display = "block";
+            }
+
         });
     }
 
